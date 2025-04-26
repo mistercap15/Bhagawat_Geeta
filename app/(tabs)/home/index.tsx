fd09abd72b31691e4600be0f5941d0d5bdc3b914
Khilan Patel <khilanpatel15@gmail.com>
2025-04-27 00:19:55 +0530

    ADD

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import {
   View,
   ScrollView,
@@ -23,6 +23,12 @@ import * as Sharing from "expo-sharing";
 import * as MediaLibrary from "expo-media-library";
 import { captureRef } from "react-native-view-shot";
 import Animated, { FadeInDown } from "react-native-reanimated";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  BottomSheetModal,
+  BottomSheetView,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
 
 export default function HomeScreen() {
   const [shlokaOfTheDay, setShlokaOfTheDay] = useState<any>(null);
@@ -36,6 +42,14 @@ export default function HomeScreen() {
   const sectionBg = isDarkMode ? "bg-gray-800" : "bg-white";
 
   const viewRef = useRef<any>(null);
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
 
   const fetchShloka = async () => {
     setLoading(true);
@@ -122,174 +136,203 @@ export default function HomeScreen() {
   };
 
   return (
-    <View className={`flex-1 ${bgColor}`}>
-      <ScrollView
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-      >
-        {/* Header */}
-        <View className="items-center mt-6 mb-8">
-          <BookText size={80} color="#92400e" />
-          <Text className={`text-4xl font-bold mt-4 ${textColor}`}>
-            Bhagavad Gita
-          </Text>
-          <Text className={`text-base mt-1 ${textColor}`}>
-            Daily Wisdom for the Soul
-          </Text>
-        </View>
-
-        {/* Shloka Card */}
-        <View
-          ref={viewRef}
-          className={`mx-5 ${sectionBg} rounded-3xl shadow-md p-6 mb-8`}
+    <GestureHandlerRootView className="flex-1">
+      <View className={`flex-1 ${bgColor}`}>
+        <ScrollView
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
         >
-          <Text className={`text-xl font-semibold mb-3 ${textColor}`}>
-            📜 Shloka of the Day
-          </Text>
-          {loading ? (
-            <ActivityIndicator size="large" color="#92400e" />
-          ) : error ? (
-            <Text className={`text-base ${textColor} text-center`}>
-              No data available at the moment. Pull to refresh.
+          {/* Header */}
+          <View className="items-center mt-6 mb-8">
+            <BookText size={80} color="#92400e" />
+            <Text className={`text-4xl font-bold mt-4 ${textColor}`}>
+              Bhagavad Gita
             </Text>
-          ) : shlokaOfTheDay ? (
-            <View className="border-l-4 border-amber-500 pl-4">
-              <Text className={`text-base mb-2 ${textColor}`}>
-                Chapter {shlokaOfTheDay.chapter}, Verse {shlokaOfTheDay.verse}
-              </Text>
-              <Text
-                className={`text-2xl font-bold leading-relaxed mb-3 ${textColor}`}
-              >
-                {shlokaOfTheDay.text}
-              </Text>
-              <Text className={`text-base leading-relaxed ${textColor}`}>
-                {shlokaOfTheDay.translation}
-              </Text>
-            </View>
-          ) : null}
-          <View className="flex-row justify-end mt-4">
-            <TouchableOpacity
-              className="flex-row items-center space-x-1"
-              onPress={shareShloka} // Share the image on button click
-            >
-              <Share2 size={18} color="#92400e" />
-              <Text className="text-amber-700 font-semibold">Share</Text>
-            </TouchableOpacity>
+            <Text className={`text-base mt-1 ${textColor}`}>
+              Daily Wisdom for the Soul
+            </Text>
           </View>
-        </View>
 
-        {/* Explore Gita */}
-        <View className={`mx-5 mb-5 rounded-3xl ${sectionBg} p-4`}>
-          <Text className={`text-xl font-semibold mb-4 ${textColor}`}>
-            🌼 Explore the Gita
-          </Text>
-
-          <View className="space-y-4">
-            {/* Read Chapters */}
-            <Animated.View
-              entering={FadeInDown.duration(1200)}
-              className={`flex-row items-center ${sectionBg} rounded-3xl px-5 py-4`}
-            >
-              <View className="bg-amber-200 rounded-full p-3 mr-4 shadow-inner shadow-amber-300">
-                <BookOpen size={24} color="#92400e" />
+          {/* Shloka Card */}
+          <View
+            ref={viewRef}
+            className={`mx-5 ${sectionBg} rounded-3xl shadow-md p-6 mb-8`}
+          >
+            <Text className={`text-xl font-semibold mb-3 ${textColor}`}>
+              📜 Shloka of the Day
+            </Text>
+            {loading ? (
+              <ActivityIndicator size="large" color="#92400e" />
+            ) : error ? (
+              <Text className={`text-base ${textColor} text-center`}>
+                No data available at the moment. Pull to refresh.
+              </Text>
+            ) : shlokaOfTheDay ? (
+              <View className="border-l-4 border-amber-500 pl-4">
+                <Text className={`text-base mb-2 ${textColor}`}>
+                  Chapter {shlokaOfTheDay.chapter}, Verse {shlokaOfTheDay.verse}
+                </Text>
+                <Text
+                  className={`text-2xl font-bold leading-relaxed mb-3 ${textColor}`}
+                >
+                  {shlokaOfTheDay.text}
+                </Text>
+                <Text className={`text-base leading-relaxed ${textColor}`}>
+                  {shlokaOfTheDay.translation}
+                </Text>
               </View>
+            ) : null}
+            <View className="flex-row justify-end mt-4">
               <TouchableOpacity
-                onPress={() => router.push("/(tabs)/home/chapters")}
-                className="flex-1 justify-center"
+                className="flex-row items-center space-x-1"
+                onPress={shareShloka} // Share the image on button click
               >
-                <View>
+                <Share2 size={18} color="#92400e" />
+                <Text className="text-amber-700 font-semibold">Share</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {/* Explore Gita */}
+          <View className={`mx-5 mb-5 rounded-3xl ${sectionBg} p-4`}>
+            <Text className={`text-xl font-semibold mb-4 ${textColor}`}>
+              🌼 Explore the Gita
+            </Text>
+
+            <View className="space-y-4">
+              {/* Read Chapters */}
+              <Animated.View
+                entering={FadeInDown.duration(1200)}
+                className={`flex-row items-center ${sectionBg} rounded-3xl px-5 py-4`}
+              >
+                <View className="bg-amber-200 rounded-full p-3 mr-4 shadow-inner shadow-amber-300">
+                  <BookOpen size={24} color="#92400e" />
+                </View>
+                <TouchableOpacity
+                  onPress={() => router.push("/(tabs)/home/chapters")}
+                  className="flex-1 justify-center"
+                >
+                  <View>
+                    <Text className={`text-lg font-semibold ${textColor}`}>
+                      Read Chapters
+                    </Text>
+                    <Text className={`text-sm ${textColor} opacity-70`}>
+                      Explore all 18 chapters with meanings
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </Animated.View>
+
+              {/* Favorites */}
+              <Animated.View
+                entering={FadeInDown.duration(1800)}
+                className={`flex-row items-center ${sectionBg} rounded-3xl px-5 py-4`}
+              >
+                <View className="bg-yellow-200 rounded-full p-3 mr-4 shadow-inner shadow-yellow-300">
+                  <Star size={24} color="#b45309" />
+                </View>
+                <TouchableOpacity
+                  onPress={() => router.push("/(tabs)/home/favorite")}
+                  className="flex-1 justify-center"
+                >
                   <Text className={`text-lg font-semibold ${textColor}`}>
-                    Read Chapters
+                    Favorites
                   </Text>
                   <Text className={`text-sm ${textColor} opacity-70`}>
-                    Explore all 18 chapters with meanings
+                    Bookmark shlokas you love the most
+                  </Text>
+                </TouchableOpacity>
+              </Animated.View>
+
+              {/* Audio Recitations */}
+              <Animated.View
+                entering={FadeInDown.duration(1400)}
+                className={`flex-row items-center ${sectionBg} rounded-3xl px-5 py-4`}
+              >
+                <View className="bg-amber-200 rounded-full p-3 mr-4 shadow-inner shadow-amber-300">
+                  <Headphones size={24} color="#92400e" />
+                </View>
+                <TouchableOpacity
+                  onPress={handlePresentModalPress} // Open the bottom sheet on click
+                  className="flex-1 justify-center"
+                >
+                  <Text className={`text-lg font-semibold ${textColor}`}>
+                    Audio Recitations
+                  </Text>
+                  <Text className={`text-sm ${textColor} opacity-70`}>
+                    Listen to Sanskrit recitations anytime
+                  </Text>
+                </TouchableOpacity>
+                <View className="bg-orange-200 px-2 py-1 rounded-full">
+                  <Text className="text-xs text-orange-900 font-semibold">
+                    Coming Soon
                   </Text>
                 </View>
-              </TouchableOpacity>
-            </Animated.View>
+              </Animated.View>
 
-            {/* Favorites */}
-            <Animated.View
-              entering={FadeInDown.duration(1800)}
-              className={`flex-row items-center ${sectionBg} rounded-3xl px-5 py-4`}
-            >
-              <View className="bg-yellow-200 rounded-full p-3 mr-4 shadow-inner shadow-yellow-300">
-                <Star size={24} color="#b45309" />
-              </View>
-              <TouchableOpacity
-                onPress={() => router.push("/(tabs)/home/favorite")}
-                className="flex-1 justify-center"
+              {/* Daily Practice */}
+              <Animated.View
+                entering={FadeInDown.duration(1600)}
+                className={`flex-row items-center ${sectionBg} rounded-3xl px-5 py-4`}
               >
-                <Text className={`text-lg font-semibold ${textColor}`}>
-                  Favorites
-                </Text>
-                <Text className={`text-sm ${textColor} opacity-70`}>
-                  Bookmark shlokas you love the most
-                </Text>
-              </TouchableOpacity>
-            </Animated.View>
+                <View className="bg-orange-200 rounded-full p-3 mr-4 shadow-inner shadow-orange-300">
+                  <Sun size={24} color="#c2410c" />
+                </View>
+                <TouchableOpacity
+                  onPress={handlePresentModalPress}
+                  className="flex-1 justify-center"
+                >
+                  <Text className={`text-lg font-semibold ${textColor}`}>
+                    Daily Practice
+                  </Text>
+                  <Text className={`text-sm ${textColor} opacity-70`}>
+                    Set a daily reminder to read or reflect
+                  </Text>
+                </TouchableOpacity>
+                <View className="bg-orange-200 px-2 py-1 rounded-full ms-1">
+                  <Text className="text-xs text-orange-900 font-semibold">
+                    Coming Soon
+                  </Text>
+                </View>
+              </Animated.View>
 
-            {/* Audio Recitations */}
-            <Animated.View
-              entering={FadeInDown.duration(1400)}
-              className={`flex-row items-center ${sectionBg} rounded-3xl px-5 py-4`}
-            >
-              <View className="bg-amber-200 rounded-full p-3 mr-4 shadow-inner shadow-amber-300">
-                <Headphones size={24} color="#92400e" />
-              </View>
-              <View className="flex-1">
-                <Text className={`text-lg font-semibold ${textColor}`}>
-                  Audio Recitations
+              <Animated.View
+                entering={FadeInDown.duration(2000)}
+                className="items-center mt-8"
+              >
+                <Text className={`text-sm ${textColor} opacity-60`}>
+                  Crafted with ❤️ by{" "}
+                  <Text className="text-amber-800 font-semibold">
+                    Khilan Patel
+                  </Text>
                 </Text>
-                <Text className={`text-sm ${textColor} opacity-70`}>
-                  Listen to Sanskrit recitations anytime
-                </Text>
-              </View>
-              <View className="bg-orange-200 px-2 py-1 rounded-full">
-                <Text className="text-xs text-orange-900 font-semibold">
-                  Coming Soon
-                </Text>
-              </View>
-            </Animated.View>
-
-            {/* Daily Practice */}
-            <Animated.View
-              entering={FadeInDown.duration(1600)}
-              className={`flex-row items-center ${sectionBg} rounded-3xl px-5 py-4`}
-            >
-              <View className="bg-orange-200 rounded-full p-3 mr-4 shadow-inner shadow-orange-300">
-                <Sun size={24} color="#c2410c" />
-              </View>
-              <View className="flex-1">
-                <Text className={`text-lg font-semibold ${textColor}`}>
-                  Daily Practice
-                </Text>
-                <Text className={`text-sm ${textColor} opacity-70`}>
-                  Set a daily reminder to read or reflect
-                </Text>
-              </View>
-              <View className="bg-orange-200 px-2 py-1 rounded-full">
-                <Text className="text-xs text-orange-900 font-semibold">
-                  Coming Soon
-                </Text>
-              </View>
-            </Animated.View>
-
-            <Animated.View
-              entering={FadeInDown.duration(2000)}
-              className="items-center mt-8"
-            >
-              <Text className={`text-sm ${textColor} opacity-60`}>
-                Crafted with ❤️ by{" "}
-                <Text className="text-amber-800 font-semibold">
-                  Khilan Patel
-                </Text>
-              </Text>
-            </Animated.View>
+              </Animated.View>
+            </View>
           </View>
-        </View>
-      </ScrollView>
-    </View>
+        </ScrollView>
+      </View>
+
+      <BottomSheetModalProvider>
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          onChange={handleSheetChanges}
+          snapPoints={['40%', '60%']}
+          backgroundStyle={{
+            backgroundColor: isDarkMode ? '#333' : '#f1f5f9',
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+          }}
+        >
+          <BottomSheetView >
+            <Text
+              className={`text-lg font-semibold ${textColor} p-4`}
+            >
+              This feature is still in development. It will be available soon.
+            </Text>
+          </BottomSheetView>
+        </BottomSheetModal>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
